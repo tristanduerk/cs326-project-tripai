5c723de11ee1579ef07118a20ce0f7a4675bc3d5
Tristan Duerk <tristan.duerk@gmail.com>
2022-06-30 22:52:31 -0400

    Added Postgres database

--- server/database.js
+++ server/database.js
@@ -1,69 +1,55 @@
 import { readFile, writeFile } from 'fs/promises';
+import pg from 'pg-pool';
+const Pool = pg;
 
 /** A class representing a database to store scores */
+
 class Database {
   constructor() {
     this.path = 'lift_data.json';
+    this._pool = new Pool({
+      user: 'weightlifter',
+      database: 'weightdata',
+      password: 'password',
+      port: 5432,
+      host: 'localhost',
+    });
   }
 
-  async saveExercise(exerciseName, exerciseElements) {
-    const data = await this._read();
-    data.exercises[exerciseName] = exerciseElements;
-    await this._write(data);
-  }
-
-  async saveUser(userName, goalName) {
-    const data = await this._read();
-    let user = {};
-    user['name'] = userName;
-    user['goal'] = goalName;
-    data.users.push(user);
-    await this._write(data);
-  }
-
-  async loadExercise(exerciseName) {
-    const data = await this._read();
-    const exerciseElements = data.exercises[exerciseName];
-    return exerciseElements;
-  }
-
-  // This is a private methods. The _ prefix means that they are private.
-
-  async _read() {
+  async saveExercise(exercise, elements) {
     try {
-      const data = await readFile(this.path, 'utf8');
-      return JSON.parse(data);
-    } catch (error) {
-      return { 
-        exercises: {
-          'Bench Press': {
-            'weight': 45,
-            'sets': 3,
-            'reps': 5,
-            'success': true
-          },
-          'Back Squat': {
-            'weight': 45,
-            'sets': 3,
-            'reps': 5,
-            'success': true
-          },
-          'Deadlift': {
-            'weight': 45,
-            'sets': 3,
-            'reps': 5,
-            'success': true
-          }
-        }, 
-        goals: ['Strength', 'Hypertrophy', 'Athleticism'],
-        users: []
-      };
+      const res  = await this._pool.query('UPDATE exercises SET weight = $1, sets = $2, reps = $3 WHERE name = $4', [
+        elements.weight,
+        elements.sets,
+        elements.reps,
+        exercise
+      ]);
+    } catch (err) {
+      console.error(err);
     }
   }
 
-  async _write(data) {
-    await writeFile(this.path, JSON.stringify(data), 'utf8');
+  async saveUser(username, goal) {
+    try {
+      const res  = await this._pool.query('INSERT INTO users (users, goal) VALUES ($1, $2)', [
+        username,
+        goal
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
   }
+
+  async loadExercise(exercise) {
+    try {
+      const res  = await this._pool.query('SELECT * FROM exercises WHERE name = $1', [exercise]);
+      const row = res.rows[0];
+      return row;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
 }
 
 const database = new Database();
